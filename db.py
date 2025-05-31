4b29b4482c7193317d78fe5169cef47b71b70712
angel b <angelica.bracamonte.data24hel@edu.tucsweden.se>
2025-05-31 23:05:24 +0200

    ny

--- db.py
+++ db.py
@@ -2,16 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from modeller import Base
 
-
-
 DATABASE_URL = "postgresql+psycopg2://akkangell:Angelica.1@localhost:5432/your_database"
-
 
 engine = create_engine(DATABASE_URL)
 
 # session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#  (create tables)
+## (TUCgirls, lös: 1234, base: tucdb, 5431).
+# Function to create tables
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    try:
+        init_db()
+        print("✅ Tablas creadas correctamente en la base de datos.")
+    except Exception as e:
+        print("❌ Error al crear las tablas:", e)
